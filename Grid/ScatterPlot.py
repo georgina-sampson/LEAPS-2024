2949bd894fa369d037fd249b92d78fade6ce304b
Georgina Sampson <georginasampsonolalde@gmail.com>
2024-07-05 15:34:06 +0200

    mid

--- Grid/ScatterPlot.py
+++ Grid/ScatterPlot.py
@@ -34,8 +34,8 @@ def corrGrid(df, xaxis, yaxis, tipo: str, logxscale=False, logyscale=False):
     return fig
 
 def scatterGrid(df, xaxis, yaxis, title, logxscale=False, logyscale=False):
-    fig, axs = plt.subplots(len(xaxis), len(yaxis), figsize=(len(yaxis)*5, len(xaxis)*5))
-    fig.subplots_adjust(wspace=0.4, hspace=0.4)
+    fig, axs = plt.subplots(len(xaxis), len(yaxis), figsize=(len(physical)*4.5, len(species)*4))
+    fig.subplots_adjust(wspace=0.4, hspace=0.2, top=0.905)
 
     for i, spec in enumerate(xaxis):
         for j, phys in enumerate(yaxis):
@@ -44,31 +44,35 @@ def scatterGrid(df, xaxis, yaxis, title, logxscale=False, logyscale=False):
                             legend= 'auto' if i==0 and j==int(np.ceil(len(yaxis)/2)-1) else None)
             if logxscale: axs[i][j].set_xscale('log')
             if logyscale: axs[i][j].set_yscale('log')
-            if i==0 and j==int(np.ceil(len(yaxis)/2)-1): axs[i][j].legend(loc='upper center',ncols=len(df['runName'].unique()), bbox_to_anchor=(0.5, 1.2))
+            if i==0 and j==int(np.ceil(len(yaxis)/2)-1): axs[i][j].legend(loc='upper center',ncols=6, bbox_to_anchor=(0.5, 1.6))
     
     fig.suptitle(title, size='xx-large')
     return fig
 
 folder = '/data2/gsampsonolalde/LEAPS-2024/Grid/{}/{}/'
 species=['#CH3OH', 'CH3OH', '@CH3OH', '#SIO', 'SIO', '@SIO']
-physical=['Time', 'Density', 'gasTemp', 'av', 'zeta', 'radfield']
 
 runs = {constants.SHOCK: '2024-07-01_124848', constants.HOTCORE: '2024-07-01_134429'}
 logscales = [[False, False], ['log', False], [False, 'log'], ['log', 'log']]
 
 for tipo in runs:
+    physical=['Time', 'Density', 'gasTemp', 'av', 'zeta', 'radfield']
     if tipo == constants.SHOCK: physical.append(constants.SHOCKVEL)
-    print(runs[tipo], tipo)
+    
     tdf= pd.read_csv(folder.format(runs[tipo], "stage2_df.csv")[:-1], index_col=0)
     tdf.index = tdf['outputFile']
     li=[f for f in os.listdir(folder.format(runs[tipo], constants.PHASE2)) if 'startcollapse' not in f]
     df = pd.concat([makeDataframe(folder.format(runs[tipo], constants.PHASE2)+gg, tdf) for gg in li])
+    df=df.reset_index()
+    df.pop('index')
     df = df.loc[:,physical+species+['runName']]
     
     for logxscale, logyscale in logscales:
-        print(logxscale)
-        figName=folder.format('AnalysisPlots', tipo)+f'corrGrid{'_log' if logxscale else '_lin'}{'_log' if logyscale else '_lin'}.png'
-        corrGrid(df, species, physical, tipo, logxscale, logyscale).savefig(figName, dpi=300, bbox_inches='tight')
-        title=f'{tipo.upper()}{' log' if logxscale else ' lin'}{' log' if logyscale else ' lin'}'
-        figName=folder.format('AnalysisPlots', tipo)+f'scatterGrid{'_log' if logxscale else '_lin'}{'_log' if logyscale else '_lin'}.png'
+        print(tipo, logxscale, logyscale, 'corr')
+        figName=folder.format('AnalysisPlots', tipo)+f"corrGrid{'_log' if logxscale else '_lin'}{'_log' if logyscale else '_lin'}.png"
+        with np.errstate(divide='ignore'): corrGrid(df, species, physical, tipo, logxscale, logyscale).savefig(figName, dpi=300, bbox_inches='tight')
+
+        print(tipo, logxscale, logyscale, 'scatter')
+        title=f"{tipo.upper()}{' log' if logxscale else ' lin'}{' log' if logyscale else ' lin'}"
+        figName=folder.format('AnalysisPlots', tipo)+f"scatterGrid{'_log' if logxscale else '_lin'}{'_log' if logyscale else '_lin'}.png"
         scatterGrid(df, species, physical, title, logxscale=False, logyscale=False).savefig(figName, dpi=300, bbox_inches='tight')
